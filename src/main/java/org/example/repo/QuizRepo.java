e7a655cc07ea44128526bb179f28c2f779eeb0a9
Yaroslav Guz <aroslavguz@gmail.com>
2024-09-07 19:52:38 +0300

    Add changes in quiz repo

--- src/main/java/org/example/repo/QuizRepo.java
+++ src/main/java/org/example/repo/QuizRepo.java
@@ -1,7 +1,5 @@
 package org.example.repo;
 
-public class QuizRepo {
-    void getUserByQuizId(Long id) {
-        System.out.println("Some test user");
-    }
+public interface QuizRepo {
+    void getUserByQuizId(Long id);
 }
